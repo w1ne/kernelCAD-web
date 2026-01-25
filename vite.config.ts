4ee050f0112686caa13c93599379862c54348117
w1ne <14119286+w1ne@users.noreply.github.com>
2026-01-25 19:11:30 +0100

    fix: resolve syntax error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  react(),
-  tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
   worker: {
     format: 'es',
