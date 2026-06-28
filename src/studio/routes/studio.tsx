e31fbdf30001510416a5cd77925e2ae23af5aadf
w1ne <14119286+w1ne@users.noreply.github.com>
2026-06-28 13:48:11 +0200

    feat(auth): gate / and /studio behind StudioAuthGate

--- src/studio/routes/studio.tsx
+++ src/studio/routes/studio.tsx
@@ -2,11 +2,16 @@
 // Copyright (c) 2026 Andrii Shylenko and kernelCAD contributors
 import { createFileRoute } from '@tanstack/react-router';
 import App from '../App';
+import { StudioAuthGate } from '../StudioAuthGate';
 
 export const Route = createFileRoute('/studio')({
   component: StudioRoute,
 });
 
 function StudioRoute() {
-  return <App />;
+  return (
+    <StudioAuthGate>
+      <App />
+    </StudioAuthGate>
+  );
 }
